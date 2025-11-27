63a612e5e51c66dc100e2618eee2f3cba2fa23e6
ssanikajog <sanikajog1296@gmail.com>
2025-11-27 21:55:42 +0530

    Update empFunction.py

--- empFunction.py
+++ empFunction.py
@@ -1,11 +1,11 @@
 import sys
 
 if len(sys.argv) == 2:
-    scores = [float(arg) for arg in sys.argv[1:]]
+    scores = [float(x) for x in sys.argv[1].split()]
 else:
     scores = [50, 60, 70] 
 
-total = sum(scores)  # Use a different variable name
+total = sum(scores)
 average = total / len(scores)
 maximum = max(scores)
 minimum = min(scores)
